28118b12153ebdce296ae3e68d36cdd0d9a17618
Chetan Bohra <chetanbohra26@gmail.com>
2023-12-09 20:56:57 +0530

    Backend upload working

---
+++ s3-upload-backend/src/upload/upload.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Controller,
+  FileTypeValidator,
+  Get,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { UploadService } from './upload.service';
+
+@Controller('upload')
+export class UploadController {
+  constructor(private readonly uploadService: UploadService) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          // new MaxFileSizeValidator({ maxSize: 1000000 }),
+          // new FileTypeValidator({ fileType: 'application/csv' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    await this.uploadService.upload(file.originalname, file.buffer);
+  }
+}
